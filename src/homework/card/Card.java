a5b9e6fc26bd6db8bb7e100aabdfc5ed9f269605
ddumaa <dimaduma62@gmail.com>
2023-12-04 18:36:32 +0300

    editing work

--- src/homework/card/Card.java
+++ src/homework/card/Card.java
@@ -25,4 +25,6 @@ public class Card {
         this.cardNumber = cardNumber;
         this.money = money;
     }
+
+
 }
--- src/homework/card/CheckingCards.java
+++ src/homework/card/CheckingCards.java
@@ -3,17 +3,21 @@ package homework.card;
 import homework.client.ClientInfo;
 
 public class CheckingCards {
-   public static void checkingCardsMethod (int id1, int id2){
-        if (ClientInfo.cards.length == 0 ) {
-            System.out.println("You don't have a card");
-        } else if (id1 == id2) {
-            System.out.println("One card indicated");
-        } else if (ClientInfo.cards.length < id1 || ClientInfo.cards.length < id2){
-            System.out.println("You don't have such a card");
-        } else if (ClientInfo.cards[id1].equals(ClientInfo.cards[id2])){
-            System.out.println("The cards are the same");
-        } else {
-            System.out.println("The cards are not the same");
+    public static void checkingCardsMethod(){
+        for (int i = 0; i < ClientInfo.cards.length; i++){
+            int count = 0;
+            for (int x = 0; x < ClientInfo.cards.length; x++){
+                if (ClientInfo.cards[i].equals(ClientInfo.cards[x])) {
+                    count++;
+                }
+            }
+            if (i == ClientInfo.cards.length-1){
+                if (count > 0){
+                    System.out.println("Same cards: " + count);
+                } else {
+                    System.out.println("There are no identical cards");
+                }
+            }
         }
     }
 }
